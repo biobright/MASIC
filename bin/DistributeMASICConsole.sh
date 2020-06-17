f7eaed589259c8e3bc6446c875fe2f2b40f4132b
nate johnson <nate@biobright.com>
2020-06-17 17:01:19 -0400

    Dist script - include some docs and param files

--- bin/DistributeMASICConsole.sh
+++ bin/DistributeMASICConsole.sh
@@ -2,9 +2,12 @@
 
 mkdir -p ./Dist/MASICConsole
 
-cp Console/Release/MASIC_Console.exe ./Dist/MASICConsole
-cp Console/Release/*.dll             ./Dist/MASICConsole
+cp Console/Release/* ./Dist/MASICConsole
 cp ../Readme.md                      ./Dist/MASICConsole
 cp ../RevisionHistory.txt            ./Dist/MASICConsole
 
-rm -f MASIC_Console_Program.zip && zip -r MASIC_Console_Program.zip Dist/MASICConsole/* 
+cp ../Docs/*.txt ./Dist/MASICConsole
+cp ../Lib/RawFileReaderLicense.doc ./Dist/MASICConsole
+cp ../Docs/MASICParameters.xml ./Dist/MASICConsole
+
+rm -f Dist/MASIC_Console_Program.zip && pushd Dist/MASICConsole && zip -r ../MASIC_Console_Program.zip * && popd
